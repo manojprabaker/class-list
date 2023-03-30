95b44b4a0bc4961a35d58e7ecd217809299afe05
manojprabaker <manojprabaker999@gmail.com>
2023-03-30 09:14:34 +0530

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+let btn =document.querySelector("button");
+btn.classList.add("toggle-btn");
+let pP=document.querySelector("p");
+
+
+btn.addEventListener("click" ,togg);
+
+function togg(){
+	pP.classList.toggle("content");
+	console.log("hahha");
+}
